4ed663416f008664e667d5c2f7efc2e4bdc8b382
Vlad Polevoi <vlad.polewoi1@gmail.com>
2025-01-22 18:06:39 +0200

    feat: :sparkles: prototype test 1

--- src/index.ts
+++ src/index.ts
@@ -16,40 +16,89 @@ type Member = {
 
 let members: Member[] = [
   {
-    name: 'Vlad',
-    birthday: '2025-01-15',
+    name: 'Vlad 22',
+    birthday: '2020-01-22',
   },
   {
-    name: 'Fedor',
-    birthday: '2025-01-14',
+    name: 'Vlad 23',
+    birthday: '1998-01-23',
+  },
+  {
+    name: 'Vlad 24',
+    birthday: '2005-01-24',
+  },
+  {
+    name: 'Vlad 25',
+    birthday: '2006-01-25',
+  },
+  {
+    name: 'Vlad 26',
+    birthday: '2007-01-26',
   },
 ]
 
+let isRunning = true
+
 function checkBirthdays() {
+  const today = new Date()
+
   return members.filter((member) => {
-    return isToday(parse(member.birthday, 'yyyy-MM-dd', new Date()))
+    const birthday = parse(member.birthday, 'yyyy-MM-dd', new Date())
+
+    // Compare only month and day
+    return (
+      birthday.getMonth() === today.getMonth() &&
+      birthday.getDate() === today.getDate()
+    )
   })
+  // .map((member) => {
+  //   const birthYear = parseInt(member.birthday.split('-')[0])
+  //   const age = today.getFullYear() - birthYear
+  //   return { ...member, age }
+  // })
 }
 
-// cron every 10 seconds
-cron.schedule('*/10 * * * * *', () => {
-  const birthdays = checkBirthdays()
+cron.schedule('0 12 * * *', () => {
+  // cron.schedule('*/10 * * * * *', () => {
+  if (!isRunning) return
 
-  if (birthdays.length > 0) {
+  const membersWithBirthdays = checkBirthdays()
+
+  if (membersWithBirthdays.length > 0) {
     const chatID = process.env.CHAT_ID || ''
 
-    if (chatID) {
-      bot.api.sendMessage(chatID, 'Happy birthday!')
-    } else {
+    if (!chatID) {
       console.error('CHAT_ID is not set in environment variables')
+      return
     }
+
+    membersWithBirthdays.forEach((member) => {
+      const today = new Date()
+      const date = parse(member.birthday, 'yyyy-MM-dd', new Date())
+      const age = today.getFullYear() - date.getFullYear()
+
+      bot.api.sendMessage(chatID, `Happy birthday ${member.name}! He is ${age}`)
+    })
   }
 })
 
+bot.api.setMyCommands([
+  { command: 'stop', description: 'Stop the bot' },
+  { command: 'start', description: 'Start the bot' },
+])
+
+bot.command('stop', async (ctx) => {
+  isRunning = false
+  await ctx.reply('Bot stopped')
+})
+
+bot.command('start', async (ctx) => {
+  isRunning = true
+  await ctx.reply('Bot started')
+})
+
 // bot.command('getChatId', async (ctx) => {
-//   const chatID = ctx.chat.id
-//   console.log('Chat ID:', chatID)
-//   await ctx.reply(`Your chat ID is ${chatID}`)
+//   await ctx.reply(`Your chat ID is ${ctx.chat.id}`)
 // })
 
 async function startBot() {
