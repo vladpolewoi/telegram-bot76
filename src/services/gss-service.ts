155bf1938451169483944f841dce81ea543305e0
Vlad Polevoi <vlad.polewoi1@gmail.com>
2025-01-31 14:04:18 +0200

    refactor: :recycle: Move deno compatable lib for sheets

--- src/services/gss-service.ts
+++ src/services/gss-service.ts
@@ -1,8 +1,4 @@
-import {
-  GoogleSpreadsheet,
-  GoogleSpreadsheetRow,
-} from "npm:google-spreadsheet";
-import { JWT } from "npm:google-auth-library";
+import { GoogleAPI } from "https://deno.land/x/google_deno_integration/mod.ts";
 
 export interface MemberRow {
   name: string;
@@ -12,30 +8,30 @@ export interface MemberRow {
 
 export class GssService {
   private static instance: GssService;
-  private sheet: GoogleSpreadsheet;
+  private SHEET_ID: string;
+  private api: GoogleAPI;
 
   private constructor() {
-    const credentials = {
-      client_email: Deno.env.get("GOOGLE_SERVICE_ACCOUNT_EMAIL"),
-      private_key: Deno.env.get("GOOGLE_PRIVATE_KEY")?.replace(/\\n/g, "\n"),
-    };
+    const client_email = Deno.env.get("GOOGLE_SERVICE_ACCOUNT_EMAIL");
+    const private_key = Deno.env.get("GOOGLE_PRIVATE_KEY")?.replace(
+      /\\n/g,
+      "\n",
+    );
 
-    if (!credentials.client_email || !credentials.private_key) {
+    if (!client_email || !private_key) {
       throw new Error("Google credentials not found in environment variables");
     }
 
-    const SHEET_ID = Deno.env.get("GOOGLE_SHEET_ID");
-    if (!SHEET_ID) {
+    this.SHEET_ID = Deno.env.get("GOOGLE_SHEET_ID") || "";
+    if (!this.SHEET_ID) {
       throw new Error("GOOGLE_SHEET_ID not found in environment variables");
     }
 
-    const auth = new JWT({
-      email: credentials.client_email,
-      key: credentials.private_key,
-      scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+    this.api = new GoogleAPI({
+      email: client_email,
+      scope: ["https://www.googleapis.com/auth/spreadsheets"],
+      key: private_key,
     });
-
-    this.sheet = new GoogleSpreadsheet(SHEET_ID, auth);
   }
 
   public static getInstance(): GssService {
@@ -47,21 +43,13 @@ export class GssService {
 
   public async getMembers(): Promise<MemberRow[]> {
     try {
-      await this.sheet.loadInfo();
-      const worksheet = this.sheet.sheetsByIndex[1]; // Get first sheet
-      const rows = await worksheet.getRows();
+      const data = await this.api.get(
+        `https://sheets.googleapis.com/v4/spreadsheets/${this.SHEET_ID}/values/Amigos%20Members%20List`,
+      );
 
-      return rows.map((row: GoogleSpreadsheetRow) => {
-        const birthday = row.get("Birthday");
-        const year = birthday.split(".")[2];
-        const age = new Date().getFullYear() - Number(year);
-
-        return {
-          name: row.get("Name"),
-          birthday,
-          age,
-        };
-      });
+      return data?.values?.filter((row) => row.length).map(
+        this.parseSheetValue,
+      );
     } catch (error) {
       console.error("Failed to fetch birthdays:", error);
       throw error;
@@ -72,12 +60,24 @@ export class GssService {
     const members = await this.getMembers();
 
     const today = new Date();
-    const currentMonth = today.getMonth() + 1; // getMonth() returns 0-11
+    const currentMonth = today.getMonth() + 1;
     const currentDay = today.getDate();
 
     return members.filter((row) => {
-      const [day, month] = row.birthday.split(".").map(Number);
+      const [day, month] = row.birthday?.split(".").map(Number);
       return Number(month) === currentMonth && Number(day) === currentDay;
     });
+  }
+
+  private parseSheetValue(row: string[]): MemberRow {
+    const birthday = row[2];
+    const year = birthday?.split(".")[2];
+    const age = new Date().getFullYear() - Number(year);
+
+    return {
+      name: row[0],
+      birthday,
+      age,
+    };
   }
 }
